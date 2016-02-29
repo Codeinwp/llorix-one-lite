3e3d997964ba8b3a1125e68af09a2c800b15f90c
cristian-ungureanu <cristian@themeisle.com>
2016-02-29 16:13:56 +0200

    !!! fixed translation, remove duplicates from customizer

--- inc/customizer.php
+++ inc/customizer.php
@@ -506,7 +506,7 @@ function llorix_one_lite_customize_register( $wp_customize ) {
 		'sanitize_callback' => 'llorix_one_lite_sanitize_repeater',
 		'default' => json_encode(
 			array( 
-				array("icon_value" => "fa-envelope" ,"text" => "contact@site.com", "link" => "#",'id' => 'llorix_one_lite_56d069b78cb6e' ), 
+				array("icon_value" => "fa-envelope" ,"text" => "contact@site.com", "link" => "#",'id' => 'llorix_one_lite_56d450a72cb3a' ), 
 				array("icon_value" => "fa-map-marker" ,"text" => "Company address", "link" => "#",'id' => 'llorix_one_lite_56d069b88cb6f' ), 
 				array("icon_value" => "fa-tablet" ,"text" => "0 332 548 954", "link" => "#",'id' => 'llorix_one_lite_56d069b98cb70' ) 
 			)
@@ -610,8 +610,8 @@ function llorix_one_lite_customize_register( $wp_customize ) {
 			array
 			(
 				array('icon_value' =>'fa-facebook' , 'link' => '#', 'id' => 'llorix_one_lite_56d069b78cb6e'),
-				array('icon_value' =>'fa-twitter' , 'link' => '#', 'id' => 'llorix_one_lite_56d069b88cb6f'),
-				array('icon_value' =>'fa-google-plus-square' , 'link' => '#', 'id' => 'llorix_one_lite_56d069b58cb6d')
+				array('icon_value' =>'fa-twitter' , 'link' => '#', 'id' => 'llorix_one_lite_56d450842cb39'),
+				array('icon_value' =>'fa-google-plus-square' , 'link' => '#', 'id' => 'llorix_one_lite_56d450512cb38')
 			)
 		)
 
