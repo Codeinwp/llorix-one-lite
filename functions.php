02e7d95a36eb6cb25800d2da544874b879002fab
cristian-ungureanu <cristian@themeisle.com>
2016-11-16 16:47:39 +0200

    fix for https://github.com/Codeinwp/parallax-one-plus/issues/114 - remove upsells if plugin is installed

--- functions.php
+++ functions.php
@@ -157,7 +157,9 @@ if ( ! function_exists( 'llorix_one_lite_setup' ) ) :
 		 */
 		add_theme_support( 'eventbrite' );
 
-		require_once( trailingslashit( get_template_directory() ) . 'inc/customize-pro/class-llorix-one-lite-customize-upsell.php' );
+		if(!class_exists('Llorix_One_Plus')){
+			require_once( trailingslashit( get_template_directory() ) . 'inc/customize-pro/class-llorix-one-lite-customize-upsell.php' );
+		}
 	}
 endif; // llorix_one_lite_setup
 add_action( 'after_setup_theme', 'llorix_one_lite_setup' );
