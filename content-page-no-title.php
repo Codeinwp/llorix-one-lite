21b7b2ca2a10fe272d1c5dc17d69704f6feb529d
Andrei Băicuș <baicusandrei@me.com>
2017-02-14 17:43:58 +0200

    #141. Added Full width with no title page template.

---
+++ content-page-no-title.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template used for displaying page content in page.php
+ *
+ * @package llorix-one-lite
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<div class="entry-content content-page" itemprop="text">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'llorix-one-lite' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php edit_post_link( esc_html__( 'Edit', 'llorix-one-lite' ), '<span class="edit-link">', '</span>' ); ?>
+	</footer><!-- .fentry-footer -->
+</article><!-- #post-## -->
