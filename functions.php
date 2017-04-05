67592f43a5e333aba4892531a34bca2b6f2186d8
cristian-ungureanu <cristian@themeisle.com>
2017-04-05 16:40:04 +0300

    don't recommend companion when plus is active

--- functions.php
+++ functions.php
@@ -157,27 +157,29 @@ if ( ! function_exists( 'llorix_one_lite_setup' ) ) :
 		 */
 		add_theme_support( 'eventbrite' );
 
-/*
- * Notifications in customize
- */
-		require get_template_directory() . '/ti-customizer-notify/class-ti-customizer-notify.php';
+        /*
+         * Notifications in customize
+         */
+        if( ! defined('LLORIX_ONE_PLUS_PATH') ) {
+	        require get_template_directory() . '/ti-customizer-notify/class-ti-customizer-notify.php';
 
-		$config_customizer = array(
-			'recommended_plugins' => array(
-				'llorix-one-companion' 		=> array(
-					'recommended' => true,
-					'description' => /* translators: %1$s is the name for the theme */ sprintf( esc_html__( 'If you want to take full advantage of the options this theme has to offer, please install and activate %s','llorix-one-lite' ), sprintf( '<strong>%s</strong>', 'Llorix One Companion' ) ),
-				),
-			),
-			'recommended_actions' => array(),
-			'recommended_actions_title' => '',
-			'recommended_plugins_title' => esc_html__( 'Recommended Plugins', 'llorix-one-lite' ),
-			'install_button_label' => esc_html__( 'Install', 'llorix-one-lite' ),
-			'activate_button_label' => esc_html__( 'Activate', 'llorix-one-lite' ),
-			'deactivate_button_label' => esc_html__( 'Deactivate', 'llorix-one-lite' ),
-		);
-		Ti_Customizer_Notify::init( $config_customizer );
-
+	        $config_customizer = array(
+		        'recommended_plugins'       => array(
+			        'llorix-one-companion' => array(
+				        'recommended' => true,
+				        'description' => /* translators: %1$s is the name for the theme */
+					        sprintf( esc_html__( 'If you want to take full advantage of the options this theme has to offer, please install and activate %s', 'llorix-one-lite' ), sprintf( '<strong>%s</strong>', 'Llorix One Companion' ) ),
+			        ),
+		        ),
+		        'recommended_actions'       => array(),
+		        'recommended_actions_title' => '',
+		        'recommended_plugins_title' => esc_html__( 'Recommended Plugins', 'llorix-one-lite' ),
+		        'install_button_label'      => esc_html__( 'Install', 'llorix-one-lite' ),
+		        'activate_button_label'     => esc_html__( 'Activate', 'llorix-one-lite' ),
+		        'deactivate_button_label'   => esc_html__( 'Deactivate', 'llorix-one-lite' ),
+	        );
+	        Ti_Customizer_Notify::init( $config_customizer );
+        }
 	}
 endif; // llorix_one_lite_setup
 add_action( 'after_setup_theme', 'llorix_one_lite_setup' );
