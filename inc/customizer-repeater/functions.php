08ed7415efd4fefe641bcc2fb8ceae23586d2f71
cristian-ungureanu <cristian@themeisle.com>
2017-04-17 17:28:41 +0100

    fix and use general_repeater_is_empty function

--- inc/customizer-repeater/functions.php
+++ inc/customizer-repeater/functions.php
@@ -7,3 +7,31 @@
 
 // Require customizer functions and dependencies
 require get_template_directory() . '/inc/customizer-repeater/inc/customizer.php';
+
+/**
+ * Check if Repeater is empty
+ *
+ * @param string $llorix_one_lite_arr Repeater json array.
+ *
+ * @return bool
+ */
+function llorix_one_lite_general_repeater_is_empty( $llorix_one_lite_arr ) {
+    if ( empty( $llorix_one_lite_arr ) ) {
+        return true;
+    }
+    $llorix_one_lite_arr_decoded = json_decode( $llorix_one_lite_arr, true );
+    $not_check_keys = array( 'choice', 'id' );
+    foreach ( $llorix_one_lite_arr_decoded as $item ) {
+        foreach ( $item as $key => $value ) {
+            if ( $key === 'icon_value' && ( ! empty( $value ) && $value !== 'No icon') ) {
+                return false;
+            }
+            if ( ! in_array( $key, $not_check_keys ) ) {
+                if ( ! empty( $value ) ) {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
--- sections/llorix_one_lite_logos_section.php
+++ sections/llorix_one_lite_logos_section.php
@@ -15,7 +15,7 @@ $default = llorix_one_lite_logos_get_default_content();
 $llorix_one_lite_logos = get_theme_mod( 'llorix_one_lite_logos_content', $default );
 /* If section is not disabled */
 if ( isset( $llorix_one_lite_logos_show ) && $llorix_one_lite_logos_show != 1 ) {
-	if ( ! empty( $llorix_one_lite_logos ) ) {
+	if ( ! llorix_one_lite_general_repeater_is_empty( $llorix_one_lite_logos ) ) {
 		$llorix_one_lite_logos_decoded = json_decode( $llorix_one_lite_logos );
 		if ( ! empty( $llorix_one_lite_logos_decoded ) ) { ?>
 			<div class="clients white-bg" id="clients" role="region" aria-label="<?php echo __( 'Affiliates Logos','llorix-one-lite' ); ?>">
